606ec4b148f1a8b80dbcaef704b5157ca6979e64
Denis Chernykh <Denis_Chernykh@epam.com>
2023-04-16 23:13:26 +0600

    basic work with leaderboard page in runner

--- runner.py
+++ runner.py
@@ -1,9 +1,23 @@
-from config import segment_id
+from config import segment_id, strava_login, strava_password
+from login_page import LoginPage
 from segment_page import SegmentPage
 
-print(SegmentPage(segment_id, date_range="this_year", filter="current_year").url)
-print(SegmentPage(segment_id).url)
-print(SegmentPage(segment_id, date_range="this_year", filter="club", club_id="40126").url)
-print(SegmentPage(segment_id, date_range="today", filter="overall").url)
-print(SegmentPage(segment_id, date_range="this_week", filter="overall").url)
-print(SegmentPage(segment_id, date_range="this_month", filter="overall").url)
+from selenium import webdriver
+
+# Set the path to the chromedriver executable
+chromedriver_path_os_part = "chromedriver_mac_arm64"
+chromedriver_path = f"chrome_driver/{chromedriver_path_os_part}/chromedriver"
+# Create a new Chrome browser instance
+driver = webdriver.Chrome(chromedriver_path)
+login_page = LoginPage(driver)
+login_page.load()
+login_page.set_email(email=strava_login)
+login_page.set_password(password=strava_password)
+login_page.click_login()
+
+segment_page = SegmentPage(driver, segment_id, date_range="this_year", filter="current_year")
+segment_page.load()
+segment_page.get_leaderboard()
+
+
+driver.quit()
