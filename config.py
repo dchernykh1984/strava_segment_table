fb11ecaea2d5dc6e30ab0116cfc70c47843d738b
Denis Chernykh <Denis_Chernykh@epam.com>
2023-04-17 00:51:35 +0600

    add protocol data configuration, score calculation and groups of athletes

--- config.py
+++ config.py
@@ -1,4 +1,47 @@
+from results_processing.results_table import ResultsTable
+
+prize_fund = 100000.0
 strava_login = "my_login"
 strava_password = "my_password"
 segment_id = "7190094"
-segment_filter = {"date_range": "this_year", "filter": "current_year"}
+protocol_columns = {
+    "Rank": "rank",
+    "Name": "athlete_name",
+    "Score": "score",
+    "Reward": "reward",
+    "Link to attempt": "attempt_url",
+}
+groups = {
+    "Female": {
+        "age_group": "35_44",
+        "club_id": "40126",
+        "date_range": "this_month",
+        "filter": "club",
+        "gender": "F",
+        "weight_class": "75_84",
+    },
+    "Male": {
+        "age_group": "35_44",
+        "club_id": "40126",
+        "date_range": "this_month",
+        "filter": "club",
+        "gender": "M",
+        "weight_class": "75_84",
+    },
+    "Absolute": {
+        "age_group": "35_44",
+        "club_id": "40126",
+        "date_range": "this_month",
+        "filter": "club",
+        "gender": "all",
+        "weight_class": "75_84",
+    },
+}
+
+
+def calculate_score(results_table: ResultsTable):
+    for competitor in results_table.table:
+        competitor.score = 100.0 * results_table.get_leader().result / competitor.result
+    sum_score = sum([competitor.score for competitor in results_table])
+    for competitor in results_table.table:
+        competitor.reward = competitor.score * prize_fund / sum_score
