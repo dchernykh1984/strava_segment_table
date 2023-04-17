622fe407c9a3fd62125e938254303ba384233055
dchernykh <education03@mail.ru>
2023-04-17 18:16:09 +0600

    that's enough playing with html at this stage

--- pages/output_page.py
+++ pages/output_page.py
@@ -1,6 +1,6 @@
 def table_to_html(table_string):
     rows = table_string.split("\n")
-    html = "<table style='border-collapse: collapse; border: 1px solid black;'>\n"
+    html = "<table>\n"
     for i, row in enumerate(rows):
         if i % 2 == 1:
             bg_color = ""
@@ -8,10 +8,10 @@ def table_to_html(table_string):
             bg_color = "background-color: #f2f2f2;"
         if i == 0:
             bg_color = "background-color: #e2e2e2;"
-        html += "  <tr style='border: 1px solid black; " + bg_color + "'>\n"
+        html += "  <tr style='" + bg_color + "'>\n"
         cols = row.split("\t")
         for col in cols:
-            html += "    <td style='border: 1px solid black;'>" + col + "</td>\n"
+            html += "    <td>" + col + "</td>\n"
         html += "  </tr>\n"
     html += "</table>"
     return html
