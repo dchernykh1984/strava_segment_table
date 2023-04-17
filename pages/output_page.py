797e4f9dc661ee94d0dff17b942dbd5b5740c1d3
dchernykh <education03@mail.ru>
2023-04-17 17:55:08 +0600

    improve output table format

--- pages/output_page.py
+++ pages/output_page.py
@@ -1,11 +1,15 @@
 def table_to_html(table_string):
     rows = table_string.split("\n")
-    html = "<table>\n"
-    for row in rows:
-        html += "  <tr>\n"
+    html = "<table border='1'>\n"
+    for i, row in enumerate(rows):
+        if i % 2 == 0:
+            bg_color = ""
+        else:
+            bg_color = "background-color: #f2f2f2;"
+        html += "  <tr style='" + bg_color + "'>\n"
         cols = row.split("\t")
         for col in cols:
-            html += "    <td>" + col + "</td>\n"
+            html += "    <td style='border: 1px solid black;'>" + col + "</td>\n"
         html += "  </tr>\n"
     html += "</table>"
     return html
