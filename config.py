680b1bc19c017bf2a813f9c97fc472850db49a16
dchernykh <education03@mail.ru>
2023-04-17 19:50:24 +0600

    update score for almaty cup event 2023

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 from results_processing.group_protocol import CupTable
 from results_processing.results_table import ResultsTable
 
-prize_fund_stage = {"Female": 10.0, "Male": 100.0}
-prize_fund_total = {"Female": 90.0, "Male": 900.0}
-segment_ids = ["7190094", "7258238"]
+male_count = 1
+female_count = 0
+
+prize_fund_stage = {"Female": 100.0*female_count, "Male": 100.0*male_count}
+prize_fund_total = {"Female": 600.0*female_count, "Male": 600.0*female_count}
+segment_ids = ["7258238", "34037962","34037973", "16055469"]
 segment_protocol_columns = {
     "Rank": "rank",
     "Name": "athlete_name",
@@ -26,16 +29,16 @@ total_protocol_columns = {
 groups = {
     "Female": {
         "age_group": "35_44",
-        "club_id": "156006",
-        # "date_range": "this_month",
+        "club_id": "1125023",
+        "date_range": "this_month",
         "filter": "club",
         "gender": "F",
         "weight_class": "75_84",
     },
     "Male": {
         "age_group": "35_44",
-        "club_id": "156006",
-        # "date_range": "this_month",
+        "club_id": "1125023",
+        "date_range": "this_month",
         "filter": "club",
         "gender": "M",
         "weight_class": "75_84",
@@ -57,7 +60,8 @@ def calculate_stage_score(results_table: ResultsTable):
 
 def total_score_calculator(results_table: CupTable):
     for competitor in results_table.table:
-        competitor.cup_score = sum(competitor.stages_scores)
+        stages_scores = sorted(competitor.stages_scores, reverse=True)
+        competitor.cup_score = sum(stages_scores[:3]) if len(stages_scores) >= 3 else sum(stages_scores)
     cup_sum_score = sum([competitor.cup_score for competitor in results_table.table])
     for competitor in results_table.table:
         competitor.cup_reward = (
