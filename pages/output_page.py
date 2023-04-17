57b1884e2c36052510223f7a5c7c9b7735c60ac3
dchernykh <education03@mail.ru>
2023-04-17 18:11:13 +0600

    better html

--- pages/output_page.py
+++ pages/output_page.py
@@ -2,10 +2,12 @@ def table_to_html(table_string):
     rows = table_string.split("\n")
     html = "<table style='border-collapse: collapse; border: 1px solid black;'>\n"
     for i, row in enumerate(rows):
-        if i % 2 == 0:
+        if i % 2 == 1:
             bg_color = ""
         else:
             bg_color = "background-color: #f2f2f2;"
+        if i == 0:
+            bg_color = "background-color: #e2e2e2;"
         html += "  <tr style='border: 1px solid black; " + bg_color + "'>\n"
         cols = row.split("\t")
         for col in cols:
