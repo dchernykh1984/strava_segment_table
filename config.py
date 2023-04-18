ad5b6215d1b4632b627d440652f65f1883e74362
dchernykh <education03@mail.ru>
2023-04-18 16:43:08 +0600

    add comment

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ segment_ids = [
     "30058906",  # [1] Road race flat promzona (need to check and create my own)
     "21778768",  # [2] Road race flat almaty arena (need to check others and create my own)
     # "7258238",  #  [3 - no] Road race flat tupik/nuclear physics (need to check and create my own) - don't break road rules
-    "25080801",  # [1] MTB uphill, volnyi veter (easier - need to have a look and create my own segment
+    "25080801",  # [1] MTB uphill, volnyi veter (easier - need to have a look and create my own segment, also check if it is possible to ride there by road race bike
     "34037973",  # [2] MTB uphill Kok Zhailiao (maybe we need more easier one)
     "25901783",  # [1] MTB downhill (Evgeniy's version of downhill) - need to check and create my own segment
     "28840722",  # [2] MTB downhill, volnyi veter (easier - need to have a look is it safe, create my own segment)
